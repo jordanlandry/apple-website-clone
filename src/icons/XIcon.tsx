b80ebe26a4aa9889e559ed920af961af2904f38f
Jordan Landry <jordanlandry1999@gmail.com>
2022-12-10 15:46:27 -0500

    Added an icon I hadn't added before cuz I'm a dumbdumb

---
+++ src/icons/XIcon.tsx
@@ -0,0 +1,24 @@
+export default function XIcon() {
+  return (
+    <>
+      <span
+        style={{
+          transform: "rotate(45deg)",
+          height: "18px",
+          width: "1px",
+          backgroundColor: "#86868B",
+          display: "inline-block",
+        }}
+      />
+      <span
+        style={{
+          transform: "rotate(-45deg)",
+          height: "18px",
+          width: "1px",
+          backgroundColor: "#86868B",
+          display: "inline-block",
+        }}
+      />
+    </>
+  );
+}
